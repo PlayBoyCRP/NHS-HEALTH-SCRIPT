b49b44eba1751132372dfa4fac5f2321cdc659a2
PlayBoy <69969317+PlayBoyCRP@users.noreply.github.com>
2020-12-06 12:10:02 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# NHS-HEALTH-SCRIPT
+# NHS-Health-Script
+
+INSTALL
+
+1. Download the zip 
+2. Drag NHS into your resource folder 
+3. Ensure or start it in your server config 
+4. Change the coordinates to your desired location
+5. Enjoy!
---
+++ nhs/nhs/NHS1.lua
@@ -0,0 +1,37 @@
+Citizen.CreateThread(function()
+    local blip1 = AddBlipForCoord(-1362.82, -2848.84, 13.94)
+    SetBlipSprite(blip1, 153)
+end)
+
+local markers = {
+    {-1362.82, -2848.84, 13.94}
+}
+
+Citizen.CreateThread(function() 
+    while true do 
+        Citizen.Wait(1)
+        for k, v in pairs(markers) do 
+            local v1 = vector3(table.unpack(v))
+            DrawMarker(25, v1.x,v1.y,v1.z -0.95, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 2.5, 255, 0, 0, 150, 0, 0, 2, 0, 0, 0, false)
+            if Vdist2(GetEntityCoords(PlayerPedId()), v1) <= 1.4 then 
+                alert('Press ~INPUT_VEH_HORN~ get medical help')
+                if IsControlJustPressed(0, 51) then 
+                    SetEntityHealth(PlayerPedId(), 200)
+                    notify('~g~You have been healed')
+                end
+            end
+        end
+    end
+end)
+
+function alert(msg) 
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpTextFromStringLabel(0,0,1,-1)
+end
+
+function notify(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(true, false)
+end
